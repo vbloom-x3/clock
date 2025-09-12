a85f9d6390b77b495244da9d278ed42976f4fc80
bloom <vbloomv@proton.me>
2025-09-12 11:55:28 +0530

    Update script.js

--- src/system/script.js
+++ src/system/script.js
@@ -1,5 +1,5 @@
 let serverDate;
-
+let dateToggle = true;
 function displayTime(currentDate) {
   const day = String(currentDate.getDate()).padStart(2, "0");
   const month = String(currentDate.getMonth() + 1).padStart(2, "0");
@@ -9,15 +9,25 @@ function displayTime(currentDate) {
   const m = String(currentDate.getMinutes()).padStart(2, "0");
   const s = String(currentDate.getSeconds()).padStart(2, "0");
 
-  const formatted = `{
+  if (dateToggle) {
+    const formatted = `{
     date : ${day}/${month}/${year}
     time : ${h}:${m}:${s}
 }`;
-
+  }
+  else {
+    const formatted= `{
+    time : ${h}:${m}:${s}
+}`
+  }
   document.getElementById("preBlock").textContent = formatted;
   document.title = `${h}:${m}:${s}`;
 }
-
+document.getElementById("toggleLink").addEventListener("click", (event) => {
+  event.preventDefault();
+  showDate = !showDate;
+  displayTime();
+});
 function fetchTimeOnce() {
   const localDate = new Date();
   displayTime(localDate);
